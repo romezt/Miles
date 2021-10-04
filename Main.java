bc00fa7fb22bfbc042082336f7e8c863abe80f1a
Anton <ikkii780@mail.ru>
2021-10-04 22:52:45 +0500

    Miles1

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 4000;
+        int bonus = 20;
+        int miles = ticket / bonus;
+        System.out.println(miles);
+
+    }
+}
